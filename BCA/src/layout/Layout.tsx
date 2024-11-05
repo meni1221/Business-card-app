1e086f4e0cd456f6124a78519eba8f62cf0976f3
Meni <menil@MENI.com>
2024-11-05 11:01:49 +0200

    first commit

---
+++ BCA/src/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './footer/Footer'
+import Main from './main/Main'
+import Header from './header/Header'
+
+export default function Layout() {
+  return (
+    <div>
+      <Footer/>
+      <Header/>
+      <Main/>
+
+    </div>
+  )
+}
