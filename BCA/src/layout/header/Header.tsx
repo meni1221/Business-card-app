9687698b4a9d9475edc4315cb0f90c9825886ba0
Meni <menil@MENI.com>
2024-11-11 00:26:26 +0200

    Added links to the header

--- BCA/src/layout/header/Header.tsx
+++ BCA/src/layout/header/Header.tsx
@@ -1,11 +1,19 @@
+import { NavLink } from "react-router-dom";
 
 
 
 export default function Header() {
   return (
     <>
-      <header>
+      <header >
         <h1>Business card app</h1>
+    <div className="NavLink">    
+        <NavLink to="/users">Home</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="About">About</NavLink>
+        <NavLink to="login">Login</NavLink>
+    </div>
+
       </header>
     </>
   )
