4cfce5bd359dd2e2991806a2e811be2ca69c3525
Meni <menil@MENI.com>
2024-11-11 00:30:08 +0200

    Created function to add favorites

--- BCA/src/users/DisplayUsers.tsx
+++ BCA/src/users/DisplayUsers.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
-import EditUser from "./EditUser"
+
+import { NavLink } from "react-router-dom";
 
 interface User{
     id?:string
@@ -9,30 +10,33 @@ interface User{
     img:string,
 }
 interface Props{
-    users:User[],
-    deleteUser:(id:string)=>void
-    updateUser:(user:User)=>void
+    users:User[];
+    deleteUser:(id:string)=>void;
+    updateUser:(user:User)=>void;
+    AddStar:(star:User)=>void;
 }
 
 export default function DisplayUsers(props:Props) {
     const [flag,setFlag]=useState(false)
 
+
   return (
     <>
+<button><NavLink to={"/users/adduser"}>addusers</NavLink></button>
+<br />
     {props.users.map(user=>
         <div className='card' key={user.id}>
-            <h3>userName: {user.username}</h3>
-            <p>email: {user.email}</p>
-            <p>age: {user.age}</p>
+            <h3> {user.username}</h3>
+            <p> {user.email}</p>
+            <p> {user.age}</p>
             <img 
             src={user.img} 
             alt="error" 
             style={{maxWidth:"150px"}}
             />
-            <p>id:{user.id}</p>
             <button onClick={()=>{props.deleteUser(user.id!)}}>Delete</button>
-            <button onClick={()=>setFlag(true)}>Edit</button>
-            {flag && <EditUser editUser={props.updateUser} user={user} />}
+            <button onClick={()=>{props.updateUser(user)}}><NavLink to={"/users/edit"}>edit</NavLink></button>
+            <button onClick={()=>{props.AddStar(user)}}>AddStar</button>
         </div>
     )}
     </>
