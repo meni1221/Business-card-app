d722c663c7e2eef8141084fb08b3bac7804081aa
Meni <menil@MENI.com>
2024-11-11 00:25:45 +0200

    created Props teh children from ReactNode

--- BCA/src/layout/Layout.tsx
+++ BCA/src/layout/Layout.tsx
@@ -3,7 +3,10 @@ import Main from './main/Main'
 import Footer from './footer/Footer'
 import { useState } from 'react'
 
-export default function Layout() {
+interface Props{
+  children:React.ReactNode
+}
+export default function Layout({children}:Props) {
 
 
   const [name, setName] = useState("Nimrod")
@@ -16,7 +19,7 @@ export default function Layout() {
       {/* השמה לפרטי השם של האבא אצל הבן  */}
       {/* <BrederOfMain name = {name} /> */}
       {/* הכנסת הקונבציה של השם של האבא אצל הבן */}
-      <Main />
+      <Main children={children}/>
       <Footer/>
       </div>
     </>
