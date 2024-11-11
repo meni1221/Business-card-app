27a8cb7b791a10f1c784cd023e35a2e67b27ec89
Meni <menil@MENI.com>
2024-11-12 00:14:23 +0200

    Implemented use of context

--- BCA/src/users/DisplayUsers.tsx
+++ BCA/src/users/DisplayUsers.tsx
@@ -1,44 +1,61 @@
-import { useState } from "react"
+import { useContext } from "react";
 
 import { NavLink } from "react-router-dom";
+import PageHeader from "../components/PageHeader";
+import { UserContext } from "../Providers/UserProvider";
 
-interface User{
-    id?:string
-    username:string,
-    email:string,
-    age:number,
-    img:string,
-}
-interface Props{
-    users:User[];
-    deleteUser:(id:string)=>void;
-    updateUser:(user:User)=>void;
-    AddStar:(star:User)=>void;
+interface User {
+  id?: string;
+  username: string;
+  email: string;
+  age: number;
+  img: string;
 }
 
-export default function DisplayUsers(props:Props) {
-    const [flag,setFlag]=useState(false)
+interface Props {
+  deleteUser: (id: string) => void;
+  AddStar: (star: User) => void;
+}
 
+export default function DisplayUsers(props: Props) {
+  const { users, setUsers } = useContext(UserContext);
 
   return (
     <>
-<button><NavLink to={"/users/adduser"}>addusers</NavLink></button>
-<br />
-    {props.users.map(user=>
-        <div className='card' key={user.id}>
-            <h3> {user.username}</h3>
-            <p> {user.email}</p>
-            <p> {user.age}</p>
-            <img 
-            src={user.img} 
-            alt="error" 
-            style={{maxWidth:"150px"}}
-            />
-            <button onClick={()=>{props.deleteUser(user.id!)}}>Delete</button>
-            <button onClick={()=>{props.updateUser(user)}}><NavLink to={"/users/edit"}>edit</NavLink></button>
-            <button onClick={()=>{props.AddStar(user)}}>AddStar</button>
+      <PageHeader title="Users pages" subtitle="welcome users pages" />
+      <button>
+        <NavLink to={"/users/adduser"}>addusers</NavLink>
+      </button>
+      <br />
+      {users!.map((user) => (
+        <div className="card" key={user.id}>
+          <img src={user.img} alt="error" style={{ maxWidth: "150px" }} />
+          <h3> {user.username}</h3>
+          <p> {user.email}</p>
+          <p> {user.age}</p>
+          <button
+            onClick={() => {
+              setUsers(users.filter((i) => i.id !== user.id));
+            }}
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => {
+              setUsers(users.map((itemUser) => (itemUser.id === user.id ? user :itemUser)))
+            }}
+          >
+            <NavLink to={`/users/edit/${user.id}`}>edit</NavLink>
+          </button>
+          <button
+            onClick={() => {
+              props.AddStar(user);
+            }}
+          >
+            AddStar
+          </button>
         </div>
-    )}
+      ))}
     </>
-  )
+  );
 }
