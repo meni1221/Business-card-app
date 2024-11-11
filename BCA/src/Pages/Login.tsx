c4d2f139c114ebc87c0581c29fc6d3fea46b3b84
Meni <menil@MENI.com>
2024-11-12 00:12:56 +0200

    Implemented use of PageHeader

--- BCA/src/Pages/Login.tsx
+++ BCA/src/Pages/Login.tsx
@@ -1,35 +1,37 @@
-import { useState } from "react"
+import { useState } from "react";
+import PageHeader from "../components/PageHeader";
 
 export default function Login() {
-    const [username, setusername] = useState("")
-    const [password, setPassword] = useState("")
+  const [username, setusername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <>
-    <p>
-     <label htmlFor="userName"><h3>user Name</h3></label>
-      <input
-       id='userName'
-       type="text"
-       value={username}
-       placeholder='userName'
-       onChange={(event)=>
-        setusername(event.target.value)
-      }
-      />
+    <form>
+      <PageHeader title="Login pages" subtitle="welcome to Business card app" />
+      <p>
+        <label htmlFor="userName">
+          <h3>user Name</h3>
+        </label>
+        <input
+          id="userName"
+          type="text"
+          value={username}
+          placeholder="userName"
+          onChange={(event) => setusername(event.target.value)}
+        />
       </p>
 
-    <p>
-     <label htmlFor="password"><h3>password</h3></label>
-      <input
-       id='password'
-       type="password"
-       value={password}
-       placeholder='password'
-       onChange={(event)=>
-        setPassword(event.target.value)
-}
-      />
+      <p>
+        <label htmlFor="password">
+          <h3>password</h3>
+        </label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </p>
-    </>
-  )
+    </form>
+  );
 }
