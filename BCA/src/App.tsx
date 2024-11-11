ffe9c748ca7d9f731eaea186f06c8cce5ef53d78
Meni <menil@MENI.com>
2024-11-12 00:08:43 +0200

    import App css

--- BCA/src/App.tsx
+++ BCA/src/App.tsx
@@ -1,14 +1,13 @@
-import "./app.css" 
-import Layout from './layout/Layout'
-import AppRouter from "./router/AppRouter"
+import "./app.css";
+import Layout from "./layout/Layout";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-
   return (
-  <>
-        <Layout children={<AppRouter/>}/>
-  </> 
-  )
+    <>
+      <Layout children={<AppRouter />} />
+    </>
+  );
 }
 
-export default App
+export default App;
